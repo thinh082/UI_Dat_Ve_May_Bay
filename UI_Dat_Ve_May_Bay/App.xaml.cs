2ba1a084e5aa8005b240ccb29165305f2d5cce22
thinh082 <158122294+thinh082@users.noreply.github.com>
2026-01-02 10:00:04 +0700

    Add project files.

---
+++ UI_Dat_Ve_May_Bay/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace UI_Dat_Ve_May_Bay;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
